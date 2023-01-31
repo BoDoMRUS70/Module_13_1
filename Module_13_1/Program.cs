c52596aea5b58105728ef3b2e36677a3ae8dd7bd
Bodomrus70 <Bodomrus70@mail.ru>
2023-01-31 20:50:26 +0700

    Добавьте файлы проекта.

---
+++ Module_13_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Module_13_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var strings = new List<string>();
+            string text = File.ReadAllText("C:\\Domeny\\Downloads\\Text1.txt");
+            strings.Add(text);
+            Console.WriteLine($"List Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//8ms
+
+            stopWatch = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            text = File.ReadAllText("C:\\Domeny\\Downloads\\Text1.txt");
+            LinkedList.AddFirst(text);
+            Console.WriteLine($"LinkedList Time Elapsed: {stopWatch.Elapsed.TotalMilliseconds} ms");//4ms, Two times faster.
+        }
+    }
+}
